58ae122a00c130b8724d5690bbd51559b2c9d7bc
KimCaraan <163628476+KimCaraan@users.noreply.github.com>
2024-03-16 14:23:55 +0800

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,6 @@
+var menuHolder = document.getElementById('menuHolder')
+var siteBrand = document.getElementById('siteBrand')
+function menuToggle(){
+  if(menuHolder.className === "drawMenu") menuHolder.className = ""
+  else menuHolder.className = "drawMenu"
+}
